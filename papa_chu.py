0b3344a42083c64cbd97f1e8ece024b02b17d038
kristine1k <kristine1k@ai-work.com>
2022-09-22 17:13:57 +0800

    feat: Implement modal for long confessions

--- papa_chu.py
+++ papa_chu.py
@@ -4,6 +4,8 @@ import re
 import disnake
 from disnake.ext import commands
 from dotenv import load_dotenv
+from disnake.ui import TextInput, Modal
+from disnake import TextInputStyle
 
 # Load environment variables
 load_dotenv()
@@ -54,6 +56,36 @@ async def confess(
         await inter.send(
             "Use `/set_channel` to configure which channel to send the confessions to"
         )
+
+
+@bot.slash_command()
+async def long_confess(inter: disnake.ApplicationCommandInteraction):
+    """Submit a long confession to Papa Chu (can't tag/mention others using this way)"""
+
+    # Build UI
+    components = [
+        TextInput(
+            label="Confession",
+            placeholder="Come, child. Tell me your confession...",
+            custom_id="confession",
+            style=TextInputStyle.paragraph,
+        ),
+        TextInput(
+            label="Attachment",
+            placeholder="Link to image/GIF you want to attach to your confession",
+            custom_id="attachment",
+            style=TextInputStyle.short,
+            required=False,
+        ),
+    ]
+
+    modal = Modal(
+        title="Papa Chu's Confessional",
+        custom_id="long_confession",
+        components=components,
+        timeout=10,
+    )
+    await inter.response.send_modal(modal=modal)
 
 
 @bot.slash_command(default_member_permissions=disnake.Permissions(manage_guild=True))
@@ -157,46 +189,35 @@ async def on_ready():
     print(f"{bot.user.name} is live!")
 
 
+@bot.event
+async def on_modal_submit(inter: disnake.ModalInteraction):
+    if "long_confession" == inter.custom_id:
+        # Set parameters like in /confess
+        confession = inter.text_values.get("confession")
+        attachment = inter.text_values.get("attachment")
+
+        # /confess code without tag_others
+        bot_reply = f"{inter.author.mention} confesses:"
+        embed = embed_builder(confession, attachment)
+
+        channel_id = get_channel_id()
+        if channel_id:
+            channel = bot.get_channel(channel_id)
+            await channel.send(bot_reply, embed=embed)
+            await inter.send(f"Confession sent in <#{channel_id}>!", ephemeral=True)
+        else:
+            await inter.send(
+                "Use `/set_channel` to configure which channel to send the confessions to"
+            )
+
+
 #############
 ## TESTING ##
 #############
 # @bot.slash_command(guild_ids=[679218449024811067])
 # async def confess_test(
 #     inter: disnake.CommandInteraction,
-#     confession: str = commands.Param(description="Your confession"),
-#     attachment: str = commands.Param(
-#         description="Link to image/GIF you want to attach to your confession",
-#         default=None,
-#     ),
-#     tag_others: bool = commands.Param(
-#         description="Tag/mention those in your confession (default: True)", default=True
-#     ),
 # ):
-#     """Submit a confession to Papa Chu"""
-#     mentions = get_mentions(confession)
-
-#     bot_reply = ""
-#     if tag_others and mentions:
-#         bot_reply = bot_reply + f"(cc {mentions})\n\n"
-
-#     bot_reply = bot_reply + f"{inter.author.mention} confesses:"
-#     embed = embed_builder(confession, attachment)
-
-#     channel_id = get_channel_id()
-#     if channel_id:
-#         channel = bot.get_channel(channel_id)
-#         await channel.send(bot_reply, embed=embed)
-#         await inter.send(f"Confession sent in <#{channel_id}>!", ephemeral=True)
-#     else:
-#         await inter.send(
-#             "Use `/set_channel` to configure which channel to send the confessions to"
-#         )
-
-
-# @bot.slash_command(guild_ids=[679218449024811067])
-# async def test(inter: disnake.CommandInteraction):
-#     """TEST"""
-#     await inter.send(f"{get_channel_file()}")
 
 
 #######################
